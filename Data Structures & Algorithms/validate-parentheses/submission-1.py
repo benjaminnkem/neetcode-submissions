2ee932c64af67fcbcae13cf44ae1d0afcda82697
Benjamin Nkem <benjaminnkemfrancis@gmail.com>
2026-06-13 10:16:08 +0100

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        open_to_close = {
+            "{": "}",
+            "[": "]",
+            "(": ")"
+        }
+
+        stack = []
+        for bracket in s:
+            if bracket not in open_to_close:
+                if not stack:
+                    return False
+                top = stack.pop()
+            else:
+                stack.append(bracket)
+        
+        if stack:
+            return False
+        return True
+
+
+        
+            
+
